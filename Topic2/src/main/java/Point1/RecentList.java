f088dc6c852ad45e167813ff17ee2190537f356c
Rita Echeverria <ritacecheverria@gmail.com>
2019-02-05 20:20:45 -0300

    Topic 2 Point 1 initiated

---
+++ Topic2/src/main/java/Point1/RecentList.java
@@ -0,0 +1,9 @@
+package Point1;
+
+import java.util.BitSet;
+
+public class RecentList {
+
+  public void getRecentList() {
+  }
+}
---
+++ Topic2/src/test/java/Point1/RecentListTest.java
@@ -0,0 +1,16 @@
+package Point1;
+
+import Point1.RecentList;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RecentListTest {
+
+  @Test
+  public void emptyRecentList() {
+    RecentList recentList = new RecentList ();
+    assertTrue (RecentList.getRecentList().isEmpty());
+  }
+}
