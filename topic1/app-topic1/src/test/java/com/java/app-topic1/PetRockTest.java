bb6657b3a2ef874aa064ce7d4b75b24962c93a03
Rita Echeverria <ritacecheverria@gmail.com>
2019-02-19 14:19:27 -0300

    topic 1 fixed

--- topic1/app-topic1/src/test/java/com/java/app-topic1/PetRockTest.java
+++ topic1/app-topic1/src/test/java/com/java/app-topic1/PetRockTest.java
@@ -1,9 +1,8 @@
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class PetRockTest {
-
+class PetRockTest {
   @Test
   public void getName() {
     PetRock rock = new PetRock ("Rocky");
